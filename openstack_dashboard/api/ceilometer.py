71a61a57764d6894406444f92b11f7bc56539c6a
Yolanda Robla <yolanda.robla@canonical.com>
2013-03-08 09:50:57 +0000

    Fixing getting usage for each resource.

--- openstack_dashboard/api/ceilometer.py
+++ openstack_dashboard/api/ceilometer.py
@@ -132,13 +132,10 @@ def global_usage(request, fields):
 
     filtered = filter(lambda m: m.name in fields, meters)
 
-    def get_query(user, project):
-        query = [{"field": "user",
+    def get_query(resource):
+        query = [{"field": "resource",
                   "op": "eq",
-                  "value": user},
-                 {"field": "project",
-                  "op": "eq",
-                  "value": project}]
+                  "value": resource}]
         return query
 
     usage_list = []
@@ -159,7 +156,7 @@ def global_usage(request, fields):
 
     for m in filtered:
         statistic = statistic_get(request, m.name,
-            query=get_query(m.user_id, m.project_id))
+            query=get_query(m.resource_id))
         usage_list.append({"tenant": get_tenant(m.project_id),
                       "user": get_user(m.user_id),
                       "total": statistic.max,
